fc7901f05318fe4643f28125f56c27683a7c0ce9
Gia <gjackson24@scienceleadership.org>
2023-03-16 10:09:44 -0400

    View

--- src/main/java/com/example/inventoryofvendingmachineapp/HelloController.java
+++ src/main/java/com/example/inventoryofvendingmachineapp/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.inventoryofvendingmachineapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    public Button Pistachios;
+    public Button ChocolateChippers;
+    public Button Goldfish;
+    public Button Funyuns;
+    public Button DoritosFlaminHot;
+    public Button WhiteCheddarPopcorn;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
