44480347fc0b083d45bae872c9d2d03e0dd0590d
ChrisStraface <chstraface@gmail.com>
2023-03-16 10:12:26 -0400

    buttons

--- src/main/java/com/example/inventoryofvendingmachineapp/HelloController.java
+++ src/main/java/com/example/inventoryofvendingmachineapp/HelloController.java
@@ -4,9 +4,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+
+
 public class HelloController {
     @FXML
     private Label welcomeText;
+    public Button Doritos;
+    public Button FruitSnacks;
+    public Button NatureValley;
+    public Button Takis;
+    public Button RiceKrispies;
+    public Button Popcorners;
     public Button Pistachios;
     public Button ChocolateChippers;
     public Button Goldfish;
@@ -17,4 +25,5 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
