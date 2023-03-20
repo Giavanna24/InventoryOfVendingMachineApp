19854ff66a0643815854ef58a349e03d2734824b
ChrisStraface <chstraface@gmail.com>
2023-03-20 14:41:32 -0400

    tried tabs again

--- src/main/java/com/example/inventoryofvendingmachineapp/HelloController.java
+++ src/main/java/com/example/inventoryofvendingmachineapp/HelloController.java
@@ -3,9 +3,14 @@ package com.example.inventoryofvendingmachineapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
 public class HelloController {
 
     @FXML
+    //Vending machine items
     private Label welcomeText;
     public Button Doritos;
     public Button FruitSnacks;
@@ -19,6 +24,18 @@ public class HelloController {
     public Button Funyuns;
     public Button DoritosFlaminHot;
     public Button WhiteCheddarPopcorn;
+    // Model elements
+    public GridPane MachineGrid;
+    public Pane MachinePane;
+    public Label TitleLabel;
+    public Label InsertLabel;
+    public Label TotalLabel;
+    public Label ItemLabel;
+    public Label StockLabel;
+    public Label PriceLabel;
+    public TextField TotalField;
+    public TextField InsertField;
+    public Button CheckButton;
 
     public void initialize(){
         Model.readAllData();
